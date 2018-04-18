ade83affbfec0a3bc227c0a2a6844ab4d18a72ae
andregeuze <andregeuze@gmail.com>
2018-04-18 11:40:55 +0200

    add self contained option to publish

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,10 @@
 Param(
     $configuration = "Release",
-    $publishUrl = "$PSScriptRoot\_publish"
+    $selfContained = $False
 )
+
+$publishUrl = "$PSScriptRoot\_publish"
+$publishUrl_sc = "$PSScriptRoot\_publish_sc"
 
 $msbuildCommand = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
@@ -10,6 +13,11 @@ $msbuildCommand = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterpris
 New-Item -ItemType Directory -Path "$publishUrl\Bridge" -Force -Verbose
 Copy-Item -Path ".\Bridge\**\bin\**\$configuration\*.dll" -Destination "$publishUrl\Bridge" -Force -Recurse -Verbose
 
-dotnet publish .\Receiver\Receiver\Receiver.csproj -c $configuration -o "$publishUrl\Receiver" --self-contained -r win10-x64
+dotnet publish .\Receiver\Receiver\Receiver.csproj -c $configuration -o "$publishUrl\Receiver"
+dotnet publish .\Dashboard\Dashboard.sln -c $configuration -o "$publishUrl\Dashboard"
 
-dotnet publish .\Dashboard\Dashboard.sln -c $configuration -o "$publishUrl\Dashboard" --self-contained -r win10-x64
+if ($selfContained)
+{
+    dotnet publish .\Receiver\Receiver\Receiver.csproj -c $configuration -o "$publishUrl_sc\Receiver" --self-contained -r win10-x64
+    dotnet publish .\Dashboard\Dashboard.sln -c $configuration -o "$publishUrl_sc\Dashboard" --self-contained -r win10-x64
+}
