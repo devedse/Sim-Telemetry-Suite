796ab94222e804ecda62e80f8723168c5cb7f43a
andregeuze <andregeuze@gmail.com>
2018-04-18 11:03:03 +0200

    update publish script

--- publish.ps1
+++ publish.ps1
@@ -1,7 +1,7 @@
-cls
-
-$configuration = "Release"
-$publishUrl = "$PSScriptRoot\_publish"
+Param(
+    $configuration = "Release",
+    $publishUrl = "$PSScriptRoot\_publish"
+)
 
 $msbuildCommand = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
@@ -10,12 +10,6 @@ $msbuildCommand = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterpris
 New-Item -ItemType Directory -Path "$publishUrl\Bridge" -Force -Verbose
 Copy-Item -Path ".\Bridge\**\bin\**\$configuration\*.dll" -Destination "$publishUrl\Bridge" -Force -Recurse -Verbose
 
-dotnet publish .\Receiver\Receiver\Receiver.csproj -c $configuration -o "$publishUrl\Receiver"
+dotnet publish .\Receiver\Receiver\Receiver.csproj -c $configuration -o "$publishUrl\Receiver" --self-contained -r win10-x64
 
-dotnet msbuild .\dashboard\Dashboard.sln `
-    /p:Configuration=$configuration `
-    /p:DeployOnBuild=True `
-    /p:DeployDefaultTarget=WebPublish `
-    /p:WebPublishMethod=FileSystem `
-    /p:DeleteExistingFiles=True `
-    /p:publishUrl=$publishUrl\Dashboard
+dotnet publish .\Dashboard\Dashboard.sln -c $configuration -o "$publishUrl\Dashboard" --self-contained -r win10-x64
